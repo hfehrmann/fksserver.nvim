efb41bbe31c98a47ca85d0dcddf91e0386f68832
Hans Fehrmann <hans.jfehrmann@gmail.com>
2026-07-05 01:08:10 -0700

    Added FKSInstall link of the tool

--- lua/fksserver/fksnv.lua
+++ lua/fksserver/fksnv.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local FKSNV_PATH = "/usr/local/bin/fksnv"
+
 function M.exists()
     return vim.fn.executable("fksnv") == 1
 end
@@ -7,14 +9,14 @@ end
 function M.check()
     local exists = M.exists()
     if not exists then
-        vim.notify("The fksnv shell command is not installed. Install it with FKSInstall.", vim.log.levels.WARN)
+        vim.notify("The fksnv shell command is not installed. Run FKSInstall.", vim.log.levels.WARN)
     end
 end
 
 function M.install()
     local scripts_folders = vim.api.nvim_get_runtime_file("scripts/", true)
 
-    local fksserver_path = ""
+    local fksserver_path = nil
     for _, file in ipairs(scripts_folders) do
         if file:find("fksserver.nvim", 1, true) ~= nil then
             fksserver_path = file
@@ -22,7 +24,33 @@ function M.install()
         end
     end
 
-    print("TODO: install command")
+    if fksserver_path == nil then
+        vim.notify("Couldn't find fksserver.nvim plugin path", vim.log.levels.ERROR)
+        return
+    end
+    local fksnv_script = fksserver_path .. "fksnv.sh"
+
+    local warning = "" ..
+        "You are about to install the fksnv binary into " .. FKSNV_PATH .. "\n" ..
+        "This requires sudo access."
+    print(warning)
+
+    local password = vim.fn.inputsecret("Password: ")
+    vim.cmd("redraw!") -- Clear command line
+
+    if password == "" then
+        vim.notify("No password provided.", vim.log.levels.WARN)
+        return
+    end
+
+    vim.fn.system({ "sudo", "-S", "ln", "-s", fksnv_script, FKSNV_PATH }, password)
+
+    local fksnv_exists = M.exists()
+    if fksnv_exists then
+        vim.notify("Success!")
+    else
+        vim.notify("fksnv was not installed.", vim.log.levels.ERROR)
+    end
 end
 
 return M
